09bf7cfd929d0e2c5fa856ec119af51fb63a2562
Matthias P. Braendli (think) <matthias@mpb.li>
2013-11-08 18:56:13 +0100

    update zmq-rx.py, can be used to pipe ETI into dabmod.

--- zmq-rx.py
+++ zmq-rx.py
@@ -1,7 +1,37 @@
 #!/usr/bin/env python
+#   Copyright (C) 2013 Matthias P. Braendli
+#   http://mpb.li
+#
+#   This file is part of CRC-DabMux.
+#
+#   CRC-DabMux is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   CRC-DabMux is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with CRC-DabMux.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# Use this script to receive the zmq+tcp://*:8080 output from
+# crc-dabmux.
+#
+# It can then be piped into CRC-DabMod. Thanks to ZeroMQ, the connection
+# stays up even when crc-dabmux is killed and restarted. However,
+# frame-sync is lost, and UHD Underruns will happen.
+
+# This is python3 code, it will probably not work on python2.
+# TODO fix that
 
 import sys
 import zmq
+
+RAW_LEN=6144
 
 context = zmq.Context()
 
@@ -9,11 +39,12 @@ sock = context.socket(zmq.SUB)
 
 sock.connect("tcp://localhost:8080")
 
+# set a filter that lets everything through
 sock.setsockopt(zmq.SUBSCRIBE, bytes([]))
 
-print("Entering loop")
-for i in range(10):
+sys.stderr.write("Entering loop\n")
+while True:
     data = sock.recv(flags=0, copy=True, track=False)
-    print("RX {0}B, {1:2x}, {2:2x}, {3:2x}, {4:2x}, ...".format(
-        len(data),
-        data[0], data[1], data[2], data[3] ))
+
+    sys.stdout.buffer.write(data)
+    sys.stdout.buffer.write(bytes([0x55] * (RAW_LEN - len(data))))
