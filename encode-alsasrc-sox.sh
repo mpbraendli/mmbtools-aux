99f0e62869a0ce274594760c30705fe13d87dad9
Matthias P. Braendli <matthias.braendli@mpb.li>
2014-01-05 16:16:48 +0100

    add encode-alsasrc-sox.sh

---
+++ encode-alsasrc-sox.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Read audio from ALSA input using sox, and encode with fdk-aac-dabplus-zmq
+#
+BITRATE=$1
+DST=$2
+ALSASRC="default"
+
+if [ "$DST" == "" ]
+then
+    echo "Usage:"
+    echo " $0 <bitrate> <zmq destination>"
+    exit 1
+fi
+
+sox -t alsa $ALSASRC -b 16 -t raw - rate 48k channels 2 | \
+    ../fdk-aac-dabplus/aac-enc-dabplus-zmq \
+    -i /dev/stdin -b $BITRATE -f raw -a -o $DST
+
