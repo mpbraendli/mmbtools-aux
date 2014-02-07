05bc267d10902357814a8493f18937f5cb51f8f6
Matthias P. Braendli <matthias.braendli@mpb.li>
2014-02-07 09:24:39 +0100

    some updates, removal of old scripts

--- moduhd-2048.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-../crc-dabmod-0.3.3/src/crc-dabmod /dev/stdin -g2 $* -l -u "master_clock_rate=32768000,type=b100" -F 234208000
--- moduhd-resample.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-../crc-dabmod-0.3.3/src/crc-dabmod /dev/stdin -g2 $* -l -r 4000000 -u "" -F 234208000
