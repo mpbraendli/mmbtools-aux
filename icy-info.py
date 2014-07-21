56bf00060ff4ca9feb1c6795693ca3585bd9f758
Matthias P. Braendli <matthias.braendli@mpb.li>
2014-07-21 22:03:18 +0200

    icy-info: disable select call

--- icy-info.py
+++ icy-info.py
@@ -14,10 +14,6 @@ import select
 import sys
 import time
 
-# quite long, because it's only a fallback, and you don't
-# want to overwrite real ICY info
-wait_timeout=600
-
 re_icy = re.compile(r"""ICY Info: StreamTitle='([^']*)'.*""")
 
 if len(sys.argv) < 3:
@@ -43,29 +39,55 @@ def new_dlstext(text):
     fd.write(text)
     fd.close()
 
+wait_timeout = 5
+nodls_timeout = 0
+
 
 while True:
-    rfds, wfds, efds = select.select( [sys.stdin], [], [], wait_timeout)
+    # readline is blocking, therefore we cannot send a default text
+    # after some timeout
+    new_data = sys.stdin.readline()
+    if not new_data:
+        break
 
-    if rfds:
-        # new data available on stdin
-        new_data = sys.stdin.readline()
+    match = re_icy.match(new_data)
 
-        if not new_data:
-            break
-
-        match = re_icy.match(new_data)
-
-        if match:
-            artist_title = match.groups()[0]
-            new_dlstext(artist_title)
-        else:
-            print("{}".format(new_data.strip()))
-
+    if match:
+        artist_title = match.groups()[0]
+        new_dlstext(artist_title)
     else:
-        # timeout reading stdin
-        new_dlstext("")
+        print("{}".format(new_data.strip()))
 
-    time.sleep(.1)
+if False:
+    # The select call creates a one ICY delay, and it's not clear why...
+    while True:
+        rfds, wfds, efds = select.select( [sys.stdin], [], [], wait_timeout)
+
+        if rfds:
+            # new data available on stdin
+            print("SELECT !")
+            new_data = sys.stdin.readline()
+            print("DATA ! {}".format(new_data))
+
+            if not new_data:
+                break
+
+            match = re_icy.match(new_data)
+
+            if match:
+                artist_title = match.groups()[0]
+                new_dlstext(artist_title)
+            else:
+                print("{}".format(new_data.strip()))
+
+        else:
+            # timeout reading stdin
+            nodls_timeout += 1
+
+            if nodls_timeout == 100:
+                new_dlstext("")
+                nodls_timeout = 0
+
+        time.sleep(.1)
 
 
