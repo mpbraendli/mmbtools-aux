80ee87ac3e2b0d51a5d0da38e0bfbfa708261a19
Matthias P. Braendli <matthias.braendli@mpb.li>
2018-01-16 08:11:07 +0100

    Set zmq-sub Licence to MIT

--- zmqtest/zmq-sub/README.md
+++ zmqtest/zmq-sub/README.md
@@ -19,3 +19,8 @@ The tool receives from a given host and port, and outputs ETI(NI) to stdout.
 Redirect stdout to a file or another tool to save or process the data.
 
     ./zmq-sub HOST PORT > FILE.ETI
+
+LICENCE
+-------
+
+MIT, see `zmq-sub.c`
--- zmqtest/zmq-sub/zmq-sub.c
+++ zmqtest/zmq-sub/zmq-sub.c
@@ -1,3 +1,28 @@
+/*
+   Receive ODR-DabMux ZMQ ETI output and write to a file.
+
+   The MIT License (MIT)
+
+   Copyright (c) 2018 Matthias P. Braendli
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -127,7 +152,7 @@ void do_subscriber(const char* host, int port)
 void usage(char** argv)
 {
     fprintf(stderr, "usage: %s host port\n", argv[0]);
-    fprintf(stderr, "connects to dabmux ETI output at tcp://host:port using a ZeroMQ sub socket\n");
+    fprintf(stderr, "connects to odr-dabmux' ETI output at tcp://host:port using a ZeroMQ sub socket\n");
     fprintf(stderr, "and outputs raw ETI on stdout\n");
     exit(1);
 }
